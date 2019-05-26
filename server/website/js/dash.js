002ec11d6fadb9076a0448c2c1aecd88d4183286
APTXOUS <2392100311@qq.com>
2019-05-27 00:23:18 +0800

    chart

--- server/website/js/dash.js
+++ server/website/js/dash.js
@@ -15,31 +15,125 @@ function loginclick() {
 		url: "./php/login_process.php",
 		data: "username=" + user + "&password=" + pass,
 		success: function (msg) {
-			if (msg=="\nsuccess")
-			{
+			if (msg == "\nsuccess") {
 				window.location.href = "dashboard.html";
 			}
 			else
-				alert("登陆失败"+msg);
+				alert("登陆失败" + msg);
 		}
 	});
 }
 
-function getuserinfo()
-{
+function getuserinfo() {
 	$.ajax({
 		type: "POST",
 		url: "./php/dashboard.php",
-		data: JSON.stringify({                  
+		data: JSON.stringify({
 			command: "getinfo"// 注意不要在此行增加逗号
 		}),
-		dataType:"json",
+		dataType: "json",
 		success: function (msg) {
 			var nv = document.getElementById("username_p");
-			nv.innerHTML=msg.username;
+			nv.innerHTML = msg.username;
 			var nv = document.getElementById("email_h5");
-			nv.innerHTML=msg.email;
+			nv.innerHTML = msg.email;
 
 		}
 	});
 }
+var data = [];
+var data2 = [];
+var dataset;
+var totalPoints = 50;
+var updateInterval = 1000;
+var now = new Date().getTime();
+ 
+ 
+function GetData() {
+    data.shift();
+	data2.shift();
+    while (data.length < totalPoints) {     
+		var y = Math.random() * 100;
+		var tt=now += updateInterval;
+		var temp = [tt, y];
+		data.push(temp);
+		y = Math.random() * 100;
+		temp = [tt, y];
+		data2.push(temp);
+      
+	}
+	
+	
+}
+ 
+var options = {
+    series: {
+        lines: {
+            show: true,
+            lineWidth: 1.2,
+            fill: true
+        }
+    },
+    xaxis: {
+        mode: "time",
+        tickSize: [2, "second"],
+        tickFormatter: function (v, axis) {
+            var date = new Date(v);
+ 
+            if (date.getSeconds() % 20 == 0) {
+                var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+                var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+                var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+ 
+                return hours + ":" + minutes + ":" + seconds;
+            } else {
+                return "";
+            }
+        },
+        axisLabel: "Time",
+        axisLabelUseCanvas: true,
+        axisLabelFontSizePixels: 12,
+        axisLabelFontFamily: 'Verdana, Arial',
+        axisLabelPadding: 10
+    },
+    yaxis: {
+        min: 0,
+        max: 100,        
+        tickSize: 5,
+        tickFormatter: function (v, axis) {
+            if (v % 10 == 0) {
+                return v + "%";
+            } else {
+                return "";
+            }
+        },
+        axisLabel: "CPU loading",
+        axisLabelUseCanvas: true,
+        axisLabelFontSizePixels: 12,
+        axisLabelFontFamily: 'Verdana, Arial',
+        axisLabelPadding: 6
+    },
+    legend: {        
+        labelBoxBorderColor: "#fff"
+    }
+};
+ 
+$(document).ready(function () {
+    GetData();
+ 
+    dataset = [
+		{ label: "CPU1", data: data },
+		{ label: "CPU2", data: data2 }
+    ];
+ 
+    $.plot($("#flotcontainer"), dataset, options);
+ 
+    function update() {
+        GetData();
+ 
+        $.plot($("#flotcontainer"), dataset, options)
+        setTimeout(update, updateInterval);
+    }
+ 
+    update();
+});
